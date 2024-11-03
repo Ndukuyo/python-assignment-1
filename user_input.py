b1bfcc034003accfe737b3b7e445ebf9f605af59
Ndukuyo <98751668+Ndukuyo@users.noreply.github.com>
2024-11-03 18:35:15 +0300

    python user input

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("please enter your name: ")
+age = input("Please enter your age: ")
+location = input("what is your location? : ")
+print("Hi, my name is " + name + ".I am " + age + "years old, and i come from " + location)
